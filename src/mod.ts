ad82979481bf81c283e04aa7c1774263465c0782
Cj <161484149+CJ-SPT@users.noreply.github.com>
2024-04-01 20:27:55 -0400

    first commit

---
+++ src/mod.ts
@@ -0,0 +1,48 @@
+import { DependencyContainer } from "tsyringe";
+
+import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
+import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
+import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
+import { IDatabaseTables } from "@spt-aki/models/spt/server/IDatabaseTables";
+import { LogTextColor } from "@spt-aki/models/spt/logging/LogTextColor";
+
+class RemoveTimeGateFromQuests implements IPostDBLoadMod
+{
+    private logger: ILogger;
+
+    public preAkiLoad(container: DependencyContainer): void
+    {
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+    }
+
+    public postDBLoad(container: DependencyContainer): void 
+    {
+        // get database from server
+        const databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+        this.logger = container.resolve<ILogger>("WinstonLogger");
+
+        // Get all the in-memory json found in /assets/database
+        const tables: IDatabaseTables = databaseServer.getTables();
+
+        const quests = tables.templates.quests;
+
+        for (const quest in quests) 
+        {
+            const conditionsAOS = quests[quest].conditions.AvailableForStart;
+
+            if (conditionsAOS !== undefined) 
+            {
+                for (const condition in conditionsAOS)
+                {
+                    if (conditionsAOS[condition]?.conditionType === "Quest" && conditionsAOS[condition]?.availableAfter > 0)
+                    {
+                        conditionsAOS[condition].availableAfter = 0;
+                        this.logger.logWithColor(`${quests[quest].QuestName} Time requirement removed.`, LogTextColor.GREEN);
+                    }
+                }
+            }
+        }
+    }
+}
+
+module.exports = { mod: new RemoveTimeGateFromQuests() }
